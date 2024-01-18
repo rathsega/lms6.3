b0ee358ca342e6d6e35fefc4032b3c7e02a07edb
sekhar <sekhar5b0@gmail.com>
2024-01-18 10:19:04 +0530

    Prevent Zoom in and out for course watch page

--- application/views/lessons/index.php
+++ application/views/lessons/index.php
@@ -16,7 +16,9 @@ if($language_dirs){
 	<title><?php echo $course_details['title'].' | '.get_settings('system_name'); ?></title>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<!-- <meta name="viewport" content="width=device-width, initial-scale=1.0" /> -->
+	<meta name="viewport" 
+      content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 	<meta name="author" content="<?php echo get_settings('author') ?>" />
 	<meta name="keywords" content="<?php echo $course_details['meta_keywords']; ?>"/>
 	<meta name="description" content="<?php echo $course_details['meta_description']; ?>" />
