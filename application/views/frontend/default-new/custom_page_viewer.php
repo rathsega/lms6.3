cf2110768f747a3d4c1f53f49b6ec2acb20a4c32
sekhar <sekhar5b0@gmail.com>
2024-02-08 09:37:19 +0530

    Upcoming Courses Screen Image Size Issue

--- application/views/frontend/default-new/custom_page_viewer.php
+++ application/views/frontend/default-new/custom_page_viewer.php
@@ -3,3 +3,24 @@
 <div class="container py-5">
 	<?php echo remove_js(htmlspecialchars_decode_($page_content)); ?>
 </div>
+
+<style>
+    /* Common styles for all devices */
+
+    /* Styles for desktop and laptop screens */
+    @media only screen and (min-width: 1024px) {
+      /* Your desktop and laptop styles go here */
+      .container p img{
+          width: 734.609px !important;
+      }
+    }
+    
+    /* Additional styles for larger screens if needed */
+    @media only screen and (min-width: 1200px) {
+      /* Additional styles for larger screens go here */
+      .container p img{
+          width: 734.609px !important;
+      }
+    }
+
+</style>
