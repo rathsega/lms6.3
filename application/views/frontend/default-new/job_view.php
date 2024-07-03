671bbd9d1b96ee8dfe937cacc8246fbcf3223c8b
vineeth <vineeth.m@zettamine.com>
2024-07-03 10:11:34 +0530

    Jobs cards, Job listing, job view pages

--- application/views/frontend/default-new/job_view.php
+++ application/views/frontend/default-new/job_view.php
@@ -1,1 +1,332 @@
-Job View
+<!DOCTYPE html> 
+<html lang="en">
+	<head>
+
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+		<title>Dreams LMS</title>
+		
+		<!-- Favicon -->
+		<link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.svg">
+		
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+		
+		<!-- Fontawesome CSS -->
+		<link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css">
+		<link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css">
+
+		<!-- Owl Carousel CSS -->
+		<link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+		<link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+		
+		<!-- Slick CSS -->
+		<link rel="stylesheet" href="assets/plugins/slick/slick.css">
+		<link rel="stylesheet" href="assets/plugins/slick/slick-theme.css">
+		
+		<!-- Select2 CSS -->
+		<link rel="stylesheet" href="assets/plugins/select2/css/select2.min.css">
+		
+		<!-- Aos CSS -->
+		<link rel="stylesheet" href="assets/plugins/aos/aos.css">
+
+		<!-- Main CSS -->
+		<link rel="stylesheet" href="assets/css/style.css">
+	
+	</head>
+	<style>
+		.input-group img{
+			position: absolute;
+			top: 50%;
+			left: 16px;
+			transform: translateY(-50%);
+		}
+        .location-para {
+    font-size: 12px;
+    font-weight: 500;
+    padding-right: 13px;
+}
+.job-description p {
+    font-size: 14px;
+}
+ul.custom-list {
+    list-style-type: none;
+}
+ul.custom-list li {
+    color: black;
+    position: relative;
+}
+.jd-custom-list li {
+    font-size: 14px;
+    line-height: 30px;
+}
+.logo-w {
+    width: 13.66666%;
+}
+ul.custom-list li::before {
+    content: '•';
+    color: #754ffe;
+    position: absolute;
+    left: -1.5em;
+}
+.job-description-card {
+    background: #754ffe1f;
+}
+.btn-primary {
+
+    padding: 3px 32px;}
+.subs-title {
+    font-size: 18px;
+    font-weight: 700;
+    color: #392C7D;
+    margin-bottom: 20px;
+}
+.job-logo {
+    height: 80px;
+    padding: 10px;
+    border-radius: 8px;
+}
+.detail-button-2 {
+    color: #0162ff !important;
+    font-size: 10px !important;
+    font-weight: 400 !important;
+    padding: 1px 8px !important;
+    border-radius: 4px !important;
+    margin-right: 4px;
+}
+.job-details h6 {
+    font-size: 14px;
+    line-height: 4px;
+    font-weight: 600;
+    padding-top: 8px;
+    color: #754ffe;
+    padding-bottom:8px;
+}
+.job-details p {
+    font-size: 12px;
+    font-weight: 500;
+    color:#000;
+}
+	</style>
+	<body>
+
+		<!-- Main Wrapper -->
+		<div class="main-wrapper">
+			<!-- Home Banner -->
+			<section class="home-slide-2 d-flex align-items-center mt-5 pb-5">
+				<div class="container">
+					<div class="row ">
+						<div class="col-md-8">
+                        <div class="row">
+						   <div class="col-md-2 logo-w">
+                            <svg class="job-logo" viewBox="0 -13 512 512" xmlns="http://www.w3.org/2000/svg" style="background-color:#2e2882">
+                                <g fill="#feb0a5">
+                                 <path d="M256 92.5l127.7 91.6L512 92 383.7 0 256 91.5 128.3 0 0 92l128.3 92zm0 0M256 275.9l-127.7-91.5L0 276.4l128.3 92L256 277l127.7 91.5 128.3-92-128.3-92zm0 0"></path>
+                                 <path d="M127.7 394.1l128.4 92 128.3-92-128.3-92zm0 0"></path>
+                                </g>
+                                <path d="M512 92L383.7 0 256 91.5v1l127.7 91.6zm0 0M512 276.4l-128.3-92L256 275.9v1l127.7 91.5zm0 0M256 486.1l128.4-92-128.3-92zm0 0" fill="#feb0a5"></path>
+                            </svg>
+                           </div>
+                           <div class="col-md-10 mbl-conte">
+                              <h1 class="job-card-title ">UI UX Developer</h1>
+                               <img src="assets/img/office-building.svg" alt=""> <span class="text-secondary location-para">Drop Box Pvt.Ltd</span> 
+                              <img src="assets/img/loaction_pin.svg" alt=""> <span class="text-secondary location-para">Hyderabad</span> 
+                               <img src="assets/img/clock_mnth.svg" alt=""> <span class="text-secondary location-para">June 16, 2021</span>
+                              <img src="assets/img/wallet_job.svg" alt=""> <span class="text-secondary location-para">₹15,000-25,000</span>
+                              <div class="job-detail-buttons mb-3 ellipsis-line-2">
+								<button class="search-buttons detail-button">Contractual Full Time </button>
+								<button class="search-buttons detail-button">Freelance</button>
+								<button class="search-buttons detail-button">Adobe XD, Photoshaop, Sketch, Figma</button>	
+							</div>
+                           </div>
+                        </div>
+                        <div class="job-description pt-3">
+                         <h5 class="subs-title">Job description</h5>
+                         <p>UX-UI Designers are generally responsible for collecting, researching, investigating and evaluating user requirements. Their responsibility is to deliver an outstanding user experience providing an exceptional and intuitive application design.</p>
+                        </div>
+                        <div class="job-description pt-3">
+                            <h5 class="subs-title">Key responsibilities</h5>
+                            <ul class="custom-list jd-custom-list px-5">
+                                <li>You will need a copy of Adobe XD 2019 or above. A free trial can be downloaded from Adobe.</li>
+                                <li>previous design experience is needed.</li>
+                                <li>responsible for the overall feel, look, and functionality of a product from the user's perspective responsible for the overall feel, look, and functionality of a product from the user's perspective</li>
+                                <li>previous Adobe XD skills are needed.</li>
+                                <li>creating visual elements, such as logos and graphics, as well as layouts and navigation systems</li>
+                                 <li>You will need a copy of Adobe XD 2019 or above. A free trial can be downloaded from Adobe.</li>
+                                <li>previous design experience is needed.</li>
+                                <li> responsible for the overall feel, look, and functionality of a product from the user's perspective  product from the user's perspective  product from the user's perspective</li>
+                                <li>previous Adobe XD skills are needed.</li>
+                                <li>creating visual elements, such as logos and graphics, as well as layouts and navigation systems</li>
+                            
+                            </ul>
+                        </div>
+                        <div class="job-description pt-4">
+                            <h5 class="subs-title">Skill & Experience</h5>
+                            <ul class="custom-list jd-custom-list px-5">
+                                <li>You will need a copy of Adobe XD 2019 or above. A free trial can be downloaded from Adobe.</li>
+                                <li>previous design experience is needed.</li>
+                                <li>responsible for the overall feel, look, and functionality of a product from the user's perspective.</li>
+                            </ul>
+                        </div>
+						</div>
+						<div class="col-md-4">
+							 <p class="text-secondary application-para text-center pb-3">Application Ends: <span class="text-primary fw-bold">July 19, 2024</span></p>
+                             <div class="text-center">
+							<!-- Button trigger modal -->
+							<button type="button" class="btn btn-primary px-5 w-75" data-bs-toggle="modal" data-bs-target="#staticBackdrop-jobs">
+								Apply Now
+							</button>                
+                          <!-- Modal -->
+							<div class="modal fade" id="staticBackdrop-jobs" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+								<div class="modal-dialog">
+								<div class="modal-content">
+									<div class="modal-header">
+									<h1 class="modal-title fs-5" id="staticBackdropLabel">Let's Get Started</h1>
+									<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+									</div>
+									<div class="modal-body">
+										<form id="course_details_popup_form">
+											<div class="row">
+												<div class="input-group">
+													<img src="assets/img/icon/angry.svg" alt="">
+												<div class="mb-4 col-md-12">
+													<input type="text"  class="form-control" required="" name="course_details_popup_form_name" id="course_details_popup_form_name" placeholder="Name:">
+	                                            </div>
+												</div>
+												<div class="mb-4  col-md-12">
+													<input type="text" class="form-control" required="" name="course_details_popup_form_email" id="course_details_popup_form_email" placeholder="E-mail:">
+												</div>
+											</div>
+											<div class="row">
+												<div class="mb-4 col-md-12">
+													<input type="text" class="form-control" required="" name="course_details_popup_form_phone" id="course_details_popup_form_phone" placeholder="Phone No:">
+												</div>
+											</div>
+											<div class="row">
+												<div class="mb-4 col-md-12">
+												<button type="submit" class="btn btn-primary submt-btn px-4 py-2">Submit</button>
+												</div>
+											</div>
+										</form>
+									</div>
+									<div class="modal-footer">
+									</div>
+								</div>
+								</div>
+							</div>
+                             </div>
+                             <div class="job-description-card mt-4 p-3 rounded">
+                               <div class="d-flex ">
+                                  <div>
+                                      <img src="assets/img/job-calendar.svg" alt="" class="job-icons">
+                                  </div>
+                                  <div class="px-3 job-details">
+                                    <h6>Date Posted</h6>
+                                    <p>May 3, 2023</p>
+                                  </div>
+                               </div>
+                               <div class="d-flex pt-3">
+                                <div>
+                                    <img src="assets/img/job-d-location.svg" alt="" class="job-icons">
+                                </div>
+                                <div class="px-3 job-details">
+                                  <h6>Loaction</h6>
+                                  <p>Hyderabad</p>
+                                </div>
+                             </div>
+                             <div class="d-flex pt-3">
+                                <div>
+                                    <img src="assets/img/job-d-salary.svg" alt="" class="job-icons">
+                                </div>
+                                <div class="px-3 job-details">
+                                  <h6>Offered Salary</h6>
+                                  <p>₹15,000 - ₹25,000</p>
+                                </div>
+                             </div>
+                             <div class="d-flex pt-3">
+                                <div>
+                                    <img src="assets/img/hourglass.svg" alt="" class="job-icons">
+                                </div>
+                                <div class="px-3 job-details">
+                                  <h6>Expiration Date</h6>
+                                  <p>May 10, 2023</p>
+                                </div>
+                             </div>
+                             <div class="d-flex pt-3">
+                                <div>
+                                    <img src="assets/img/expertise_job.svg" alt="" class="job-icons">
+                                </div>
+                                <div class="px-3 job-details">
+                                  <h6>Experience</h6>
+                                  <p>2 Years</p>
+                                </div>
+                             </div>
+                             <div class="d-flex pt-3">
+                                <div>
+                                    <img src="assets/img/qualifucation_b.svg" alt="" class="job-icons">
+                                </div>
+                                <div class="px-3 job-details">
+                                  <h6>Qualification</h6>
+                                  <p>Any Degree</p>
+                                </div>
+                             </div>
+                             <div class="d-flex pt-3">
+                                <div>
+                                    <img src="assets/img/carrer_level.svg" alt="" class="job-icons">
+                                </div>
+                                <div class="px-3 job-details">
+                                  <h6>Carrrer Level</h6>
+                                  <p>Execuitive</p>
+                                </div>
+                             </div>
+                             </div>
+                             <div class="job-description-card mt-4 p-3 rounded">
+                                <h5 class="subs-title">Job Skills</h5>
+                                <div class="job-detail-buttons mb-3 ">
+                                    <button class="search-buttons detail-button-2 bg-light">Adobe XD </button>
+                                    <button class="search-buttons detail-button-2 bg-light">Canva </button>
+                                    <button class="search-buttons detail-button-2 bg-light">Figma </button>
+                                   <button class="search-buttons detail-button-2 bg-light">HTML, CSS, JavaScript</button>
+                                    <button class="search-buttons detail-button-2 bg-light">Photoshop</button>
+                                    <button class="search-buttons detail-button-2 bg-light">Tailwind, Advance CSS</button>
+                                    <button class="search-buttons detail-button-2 bg-light">RWD, Wordpress, UI - UX</button>
+                                 </div>
+                             </div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+	
+		
+		   
+		</div>
+	   <!-- /Main Wrapper -->
+	  
+		<!-- jQuery -->
+		<script src="assets/js/jquery-3.7.1.min.js"></script>
+		
+		<!-- Bootstrap Core JS -->
+		<script src="assets/js/bootstrap.bundle.min.js"></script>
+		
+		<!-- counterup JS -->
+		<script src="assets/js/jquery.waypoints.js"></script>
+		<script src="assets/js/jquery.counterup.min.js"></script>
+		
+		<!-- Select2 JS -->
+		<script src="assets/plugins/select2/js/select2.min.js"></script>
+
+		<!-- Owl Carousel -->
+		<script src="assets/js/owl.carousel.min.js"></script>	
+
+		<!-- Slick Slider -->
+		<script src="assets/plugins/slick/slick.js"></script>
+		
+		<!-- Aos -->
+		<script src="assets/plugins/aos/aos.js"></script>
+		
+		<!-- Custom JS -->
+		<script src="assets/js/script.js"></script>
+		
+	</body>
+</html>
